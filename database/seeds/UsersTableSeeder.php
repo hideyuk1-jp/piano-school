fb9045d38cc45be36aa57908990e46f894f1b8b0
hideyuk1-jp <hh11028+github@gmail.com>
2019-06-07 19:59:29 +0900

    usersテーブルにroleカラム追加 seederでuserテストデータ流し込み

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,30 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@sample.com',
+            'password' => bcrypt('admin'),
+            'role' => 5
+        ]);
+        // Teacher
+        for ($i=1; $i<=30; $i++) {
+            User::create([
+                'name' => 'Teacher'.$i,
+                'email' => "teacher{$i}@sample.com",
+                'password' => bcrypt("teacher{$i}"),
+                'role' => 10
+            ]);
+        }
+        // Student
+        for ($i=1; $i<=100; $i++) {
+            User::create([
+                'name' => 'Student'.$i,
+                'email' => "student{$i}@sample.com",
+                'password' => bcrypt("student{$i}"),
+                'role' => 15
+            ]);
+        }
     }
 }
