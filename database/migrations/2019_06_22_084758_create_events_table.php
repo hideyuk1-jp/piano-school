03b4f32025c8964508396ebc4d4826251fb3522e
hideyuk1-jp <hh11028+github@gmail.com>
2019-06-22 09:17:27 +0900

    まだエラーが出るため、user_idをbigInteger unsignedに変更

--- database/migrations/2019_06_22_084758_create_events_table.php
+++ database/migrations/2019_06_22_084758_create_events_table.php
@@ -15,8 +15,8 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedInteger('user_id');
-            $table->foreign('user_id')
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id') // 外部キー制約
                   ->references('id')->on('users')
                   ->onDelete('cascade');
             $table->string('title');
