6315c9049ab171cfe4853d2fbfed8e56af95ab64
hideyuk1-jp <hh11028+github@gmail.com>
2019-06-27 20:51:43 +0900

    マイグレーションファイル記述

--- database/migrations/2019_06_27_204613_create_performances_table.php
+++ database/migrations/2019_06_27_204613_create_performances_table.php
@@ -15,6 +15,27 @@ class CreatePerformancesTable extends Migration
     {
         Schema::create('performances', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->bigInteger('event_id')->unsigned();
+            $table->foreign('event_id') // 外部キー制約
+                  ->references('id')->on('events')
+                  ->onDelete('cascade');
+
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id') // 外部キー制約
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+
+            $table->bigInteger('performer_id')->unsigned();
+            $table->foreign('performer_id') // 外部キー制約
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->bigInteger('music_id')->unsigned();
+            $table->foreign('music_id') // 外部キー制約
+                ->references('id')->on('musics')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
