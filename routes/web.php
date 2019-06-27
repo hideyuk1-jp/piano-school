982f206f5e91ddf495eadfa2bbb720b25c60f9c2
hideyuk1-jp <hh11028+github@gmail.com>
2019-06-27 19:35:51 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UserController');
 
 Route::resource('events', 'EventController');
+
+Route::resource('musics', 'MusicController');
