cd4f0d80ee5089dd2d7f1fea27fc53cdc9e68b59
hideyuk1-jp <hh11028+github@gmail.com>
2019-07-19 05:27:49 +0900

    admin.indexとadmin.showの修正

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -1,32 +1,38 @@
 @extends('admin.app')
 
 @section('content')
-    <div class="d-flex align-items-end border-bottom pb-2 mb-4">
-        <h2 class="h4 mb-0">{{ __('発表会') }}</h2>
-        <a class="btn btn-primary btn-sm ml-auto" href="{{ route('admin.events.create') }}">発表会を追加</a>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>{{ __('日付') }}</th>
-                    <th>{{ __('タイトル') }}</th>
-                    <th>{{ __('作成者') }}</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($events->count() > 0)
-                    @foreach ($events as $event)
-                        <tr data-href="{{ url('admin/events/'.$event->id) }}">
-                            <td>{{ $event->date }}</td>
-                            <td>{{ $event->title }}</a></td>
-                            <td><a href="{{ url('admin/users/'.$event->user_id) }}">{{ $event->user->name }}</a></td>
+    <div class="card mb-4">
+        <div class="card-header d-flex align-items-end border-bottom">
+            <h2 class="h4 mb-0">{{ __("発表会") }}</h2>
+            <div class="ml-auto">
+                <a class="btn btn-primary btn-sm ml-auto" href="{{ route('admin.events.create') }}">{{ __('発表会を追加') }}</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>{{ __('日付') }}</th>
+                            <th>{{ __('タイトル') }}</th>
+                            <th>{{ __('作成者') }}</th>
                         </tr>
-                    @endforeach
-                @else
-                    {{ __('発表会はありません') }}
-                @endif
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        @if($events->count() > 0)
+                            @foreach ($events as $event)
+                                <tr data-href="{{ url('admin/events/'.$event->id) }}">
+                                    <td>{{ $event->date }}</td>
+                                    <td>{{ $event->title }}</a></td>
+                                    <td><a href="{{ url('admin/users/'.$event->user_id) }}">{{ $event->user->name }}</a></td>
+                                </tr>
+                            @endforeach
+                        @else
+                            {{ __('発表会はありません') }}
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/admin/events/show.blade.php
+++ resources/views/admin/events/show.blade.php
@@ -41,46 +41,51 @@
             </div>
         </div>
         <div class="card-body">
-    <dl class="row mb-0">
-        <dt class="col-md-4">{{ __('ID') }}</dt>
-        <dd class="col-md-8">{{ $event->id }}</dd>
-        <dt class="col-md-4">{{ __('タイトル') }}</dt>
-        <dd class="col-md-8">{{ $event->title }}</dd>
-        <dt class="col-md-4">{{ __('日付') }}</dt>
-        <dd class="col-md-8">{{ $event->date }}</dd>
-        <dt class="col-md-4">{{ __('詳細') }}</dt>
-        <dd class="col-md-8">{{ $event->description }}</dd>
-        <dt class="col-md-4">{{ __('作成者') }}</dt>
-        <dd class="col-md-8"><a href="{{ url('admin/users/'.$event->user_id) }}">{{ $event->user->name }}</a></dd>
-    </dl>
-    </div></div>
-
-    <h3 class="h5 mb-2">{{ __('この発表会の発表') }}</h2>
-    {{-- 発表の表示 --}}
-    <div class="table-responsive">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>{{ __('発表者') }}</th>
-                        <th>{{ __('曲名') }}</th>
-                        <th>{{ __('作曲者') }}</th>
-                        <th>{{ __('作成者') }}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if($event->performances->count() > 0)
-                        @foreach ($event->performances as $performance)
-                            <tr data-href="{{ url('admin/performances/'.$performance->id) }}">
-                                <td><a href="{{ url('admin/users/'.$performance->performer_id) }}">{{ $performance->performer->name }}</a></td>
-                                <td><a href="{{ url('admin/musics/'.$performance->music_id) }}">{{ $performance->music->title }}</a></td>
-                                <td>{{ $performance->music->composer }}</td>
-                                <td><a href="{{ url('admin/users/'.$performance->user_id) }}">{{ $performance->user->name }}</a></td>
-                            </tr>
-                        @endforeach
-                    @else
-                        {{ __('発表会はありません') }}
-                    @endif
-                </tbody>
-            </table>
+            <dl class="row mb-0">
+                <dt class="col-md-4">{{ __('ID') }}</dt>
+                <dd class="col-md-8">{{ $event->id }}</dd>
+                <dt class="col-md-4">{{ __('タイトル') }}</dt>
+                <dd class="col-md-8">{{ $event->title }}</dd>
+                <dt class="col-md-4">{{ __('日付') }}</dt>
+                <dd class="col-md-8">{{ $event->date }}</dd>
+                <dt class="col-md-4">{{ __('詳細') }}</dt>
+                <dd class="col-md-8">{{ $event->description }}</dd>
+                <dt class="col-md-4">{{ __('作成者') }}</dt>
+                <dd class="col-md-8"><a href="{{ url('admin/users/'.$event->user_id) }}">{{ $event->user->name }}</a></dd>
+            </dl>
         </div>
-    @endsection
+    </div>
+
+    {{-- 発表の表示 --}}
+    <div class="card">
+        <div class="card-body">
+            <h3 class="h5 mb-2">{{ __('この発表会の発表') }}</h3>
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>{{ __('発表者') }}</th>
+                            <th>{{ __('曲名') }}</th>
+                            <th>{{ __('作曲者') }}</th>
+                            <th>{{ __('作成者') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if($event->performances->count() > 0)
+                            @foreach ($event->performances as $performance)
+                                <tr data-href="{{ url('admin/performances/'.$performance->id) }}">
+                                    <td><a href="{{ url('admin/users/'.$performance->performer_id) }}">{{ $performance->performer->name }}</a></td>
+                                    <td><a href="{{ url('admin/musics/'.$performance->music_id) }}">{{ $performance->music->title }}</a></td>
+                                    <td>{{ $performance->music->composer }}</td>
+                                    <td><a href="{{ url('admin/users/'.$performance->user_id) }}">{{ $performance->user->name }}</a></td>
+                                </tr>
+                            @endforeach
+                        @else
+                            {{ __('発表はありません') }}
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
